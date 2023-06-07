c96ee113cba5a76ee143359871043afe85d23dbb
mhmyu <mhmyu@pm.me>
2023-06-07 08:10:56 +0800

    pengaturan deploy ke GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://ng0de.github.io',
+	base: '/website',
   integrations: [
     starlight({
       title: 'My Docs',
